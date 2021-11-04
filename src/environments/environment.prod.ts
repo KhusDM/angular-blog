941203993c381864860d9d519cf3833e28f6c0e6
husnetdinovdr <husnetdinovdr@frte.ru>
2021-11-04 23:27:56 +0300

    setting Firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
-export const environment = {
-  production: true
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: true,
+  apiKey: 'AIzaSyA_2UBF3jFbgaC7Rym2cOb-6bSia35NxDk'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,8 +2,11 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
+import { Environment } from './interface';
+
+export const environment: Environment = {
+  production: false,
+  apiKey: 'AIzaSyA_2UBF3jFbgaC7Rym2cOb-6bSia35NxDk'
 };
 
 /*
