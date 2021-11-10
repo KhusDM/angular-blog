0fe893a4ffdb059ce5a21e68a4dbbaa025d8b1eb
husnetdinovdr <husnetdinovdr@frte.ru>
2021-11-10 16:23:30 +0300

    error handling

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -14,7 +14,7 @@ export class LoginPageComponent implements OnInit {
   submitted = false;
 
   constructor(
-    private auth: AuthService,
+    public auth: AuthService,
     private router: Router
   ) {
   }
@@ -46,10 +46,14 @@ export class LoginPageComponent implements OnInit {
 
     this.auth
       .login(user)
-      .subscribe(() => {
-        this.form.reset();
-        this.router.navigate(['/admin', 'dashboard']);
-        this.submitted = false;
-      });
+      .subscribe(
+        () => {
+          this.form.reset();
+          this.router.navigate(['/admin', 'dashboard']);
+          this.submitted = false;
+        },
+        error => {
+          this.submitted = false;
+        });
   }
 }
