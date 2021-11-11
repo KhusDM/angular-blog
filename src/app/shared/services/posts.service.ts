57dd0b76475056a822c4f28ee3c13138afe14efd
husnetdinovdr <husnetdinovdr@frte.ru>
2021-11-11 17:03:44 +0300

    deleting a post

--- src/app/shared/services/posts.service.ts
+++ src/app/shared/services/posts.service.ts
@@ -37,4 +37,8 @@ export class PostsService {
           );
       }));
   }
+
+  remove(id: string): Observable<void> {
+    return this.httpClient.delete<void>(`${environment.fbDbUrl}/posts/${id}.json`);
+  }
 }
